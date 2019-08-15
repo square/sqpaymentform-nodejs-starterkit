6fae5fe02b6de37ff544a726801c473a831b2ec3
Xiao Hu <xiao@squareup.com>
2019-08-15 07:13:02 -0700

    update-to-payments-api

--- server.js
+++ server.js
@@ -12,53 +12,60 @@
 */
 const express = require('express');
 const bodyParser = require('body-parser');
+const crypto = require('crypto');
 const squareConnect = require('square-connect');
 
 const app = express();
 const port = 3000;
 
-// Set the Access Token and Location Id
+// Set the Access Token
 const accessToken = 'REPLACE_WITH_ACCESS_TOKEN';
-const locationId = 'REPLACE_WITH_LOCATION_ID';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname));
 
-// Set Square Connect credentials
+// Set Square Connect credentials and environment
 const defaultClient = squareConnect.ApiClient.instance;
 
 // Configure OAuth2 access token for authorization: oauth2
 const oauth2 = defaultClient.authentications['oauth2'];
 oauth2.accessToken = accessToken;
 
-app.post('/process-payment', function(req, res){
-  const requestParams = req.body;
+// Set 'basePath' to switch between sandbox env and production env
+// sandbox: https://connect.squareupsandbox.com
+// production: https://connect.squareup.com
+defaultClient.basePath = 'https://connect.squareupsandbox.com';
 
-  const idempotencyKey = require('crypto').randomBytes(64).toString('hex');
+app.post('/process-payment', async (req, res) => {
+  const request_params = req.body;
+
+  // length of idempotency_key should be less than 45
+  const idempotency_key = crypto.randomBytes(22).toString('hex');
 
   // Charge the customer's card
-  const transactionsApi = new squareConnect.TransactionsApi();
-  const requestBody = {
-    card_nonce: requestParams.nonce,
+  const payments_api = new squareConnect.PaymentsApi();
+  const request_body = {
+    source_id: request_params.nonce,
     amount_money: {
       amount: 100, // $1.00 charge
       currency: 'USD'
     },
-    idempotency_key: idempotencyKey
+    idempotency_key: idempotency_key
   };
-  transactionsApi.charge(locationId, requestBody).then(function(data) {
-    const json= JSON.stringify(data);
+
+  try {
+    const response = await payments_api.createPayment(request_body);
     res.status(200).json({
       'title': 'Payment Successful',
-      'result': json
+      'result': response
     });
-  }, function(error) {
+  } catch(error) {
     res.status(500).json({
       'title': 'Payment Failure',
       'result': error.response.text
     });
-  });
+  }
 });
 
 app.listen(

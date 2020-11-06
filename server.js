f41c407405f8695fa2f1a2a478b55fef9b2963c2
Xiao Hu <xiao@squareup.com>
2020-11-06 11:49:38 -0800

    Revert "Merge pull request #8 from square/jaustin/suppress-dup-payment"
    This reverts commit 876ad4aa0a75a19cca8ae70cae47bb97fedd47f6, reversing
    changes made to c59227b36bb1a19adb017a430151c4b182ac4fc2.

--- server.js
+++ server.js
@@ -40,16 +40,18 @@ defaultClient.basePath = 'https://connect.squareupsandbox.com';
 app.post('/process-payment', async (req, res) => {
   const request_params = req.body;
 
+  // length of idempotency_key should be less than 45
+  const idempotency_key = crypto.randomBytes(22).toString('hex');
+
   // Charge the customer's card
   const payments_api = new squareConnect.PaymentsApi();
   const request_body = {
     source_id: request_params.nonce,
-    location_id: request_params.location_id,
     amount_money: {
       amount: 100, // $1.00 charge
       currency: 'USD'
     },
-    idempotency_key: request_params.idempotency_key
+    idempotency_key: idempotency_key
   };
 
   try {
